acffccb130c5efca6ca6fe5ab25ed09e439cec6c
MonsieurAI <davidtomijuK@gmail.com>
2025-11-08 13:56:12 +0200

    Added local train and global test

--- fl_utils.py
+++ fl_utils.py
@@ -20,3 +20,29 @@ def federated_average(list_of_state_dicts):
         avg_state_dict[key] = layer_avg
 
     return avg_state_dict
+
+# Local training of the model using mini-batches
+def train_local(model,images,labels,numEpochs,learningRate):
+    # Define tain loader, loss function and optimizer
+    train_loader=torch.utils.data.DataLoader(images,batch_size=64,shuffle=True)
+    lossFun=nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learningRate)
+
+    #Training
+    for i in range(numEpochs):
+        for images, labels in train_loader:
+            # Forward step
+            pred=model(images)
+
+            # Compute loss
+            loss=lossFun(labels,pred)
+
+            # Backward step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+# Testing results using torchmetrics.Accuracy
+def test_global(preds, labels):
+        accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=10)
+        return accuracy(preds, labels)
