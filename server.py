f94f8d7f1be01069eadf9b07a87e71191717db31
MonsieurAI <davidtomijuK@gmail.com>
2025-11-22 11:19:27 +0200

    style: add comments, add/remove whitespaces and empty lines

--- server.py
+++ server.py
@@ -1,29 +1,35 @@
+# Import python libraries
 import socket
 import threading
 import time
-import threading
-import data_utils
-from fl_utils import federated_average
+import torch
+import torch.nn as nn
+
+# Import from custom files
+from data_utils import get_partitions
+from fl_utils import federated_average,test_global
 from network_utils import receive_data, send_data
 import model
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
 
-HOST = '127.0.0.1' # Bind to all interfeces
+# Network configuration
+HOST = '127.0.0.1'  # Bind to all interfaces
 PORT = 7878
 N_clients = 2
-num_rounds = 5
+num_rounds = 3 # Must be the same in server and client
 
+# Synchronization primitives
 start_event = threading.Event()
 end_event = threading.Event()
-
 lock = threading.Lock()
+
 list_of_state_dicts = []
+model_global = mnistNet()
 
-model_global = model.mnistNet()
-
-def client_handler(client_socket, addr, training_data, model):
+# Handles communication with a single client for the duration of training
+def client_handler(client_socket, addr, training_data, model_dict):
     try:
         # Sending the training data to clients
         send_data(client_socket, training_data, lock)
@@ -34,26 +40,33 @@ def client_handler(client_socket, addr, training_data, model):
             start_event.wait()
 
             # Sending the model to clients
-            send_data(client_socket, model, lock)
+            send_data(client_socket, model_dict, lock)
 
             # Receive weights from clients
             state_dict = receive_data(client_socket)
             with lock:
                 list_of_state_dicts.append(state_dict)
 
-            # Wait
+            # Wait until the next round (until other threads are done)
             end_event.wait()
 
+        # End of the training
+        print(f"{addr} worked good")
+
+    # Client was disconnected
     except ConnectionResetError:
         print(f"{addr} disconnected")
+
+    # Other issues
     except Exception as e:
         print(f"{addr} Error")
+
     finally:
-        print(f"{addr} worked good")
         client_socket.close()
 
 def main():
     client_sockets = 0
+
     # Creating server socket and allow to reuse port immediately
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -61,6 +74,8 @@ def main():
     # Binding
     try:
         server_socket.bind((HOST, PORT))
+
+    # Cannot bind, usually if same port is already in use
     except OSError as e:
         print("Can't bind the socket.")
         return
@@ -68,8 +83,6 @@ def main():
     # Get data for training and test
     partitions = get_partitions(N_clients)
     test_data = partitions[-1]
-
-    print(test_data)
 
     # Listening
     server_socket.listen(N_clients)
@@ -84,15 +97,18 @@ def main():
                   f"Total needed {N_clients}, {N_clients - client_sockets - 1} left")
 
             # Creating and running a new thread
-            thread = threading.Thread(target=client_handler, args=(client_sock, addr, partitions[client_sockets], model_global))
-            client_sockets+=1 # !
+            thread = threading.Thread(target=client_handler,
+                                      args=(client_sock, addr, partitions[client_sockets], model_global.state_dict()))
             thread.start()
+            client_sockets += 1
+
+    # Manual server interruption
     except KeyboardInterrupt:
         print("Server is down")
+
+    # Other issues
     except Exception as e:
         print(f"Error accepting connections: {e}")
-
-    lossFun = nn.CrossEntropyLoss()
 
     # Training loop
     try:
@@ -100,19 +116,21 @@ def main():
             print(f"Round {round + 1}")
             print("Starting training")
 
-            list_of_state_dicts.clear() # !!!
-            # Start threads
+            # Delete weights from previous rounds
+            list_of_state_dicts.clear()
+
+            # Start threads (training)
             start_event.set()
             end_event.clear()
 
-            # Wait until all threads are done
+            # Wait until all threads complete training
             while True:
                 if len(list_of_state_dicts) == N_clients:
                     break
                 time.sleep(0.1)
+            start_event.clear()
 
             # Average weights
-            start_event.clear()
             avg_state_dict = federated_average(list_of_state_dicts)
             model_global.load_state_dict(avg_state_dict)
 
@@ -124,9 +142,10 @@ def main():
 
             end_event.set()
 
+    # Other issues
     except Exception as e:
         print(f"{e}")
-        return
+
     finally:
         server_socket.close()
         print("Server shut down")
